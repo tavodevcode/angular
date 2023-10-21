1e0e5a82501a3c9b3338ca09b2a2a4df64a04aa4
tavodevcode <tavodevcode@gmail.com>
2023-10-20 23:55:30 -0600

    📝 Add new components in module for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { FormsModule } from '@angular/forms'
+import { HomeComponent } from './home/home.component'
+import { NavigationComponent } from './navigation/navigation.component'
+import { NavMenuComponent } from './nav-menu/nav-menu.component'
+import { ContentComponent } from './content/content.component'
+import { PetsComponent } from './pets/pets.component'
+import { NewPetComponent } from './pets/new-pet/new-pet.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    NavMenuComponent,
+    ContentComponent,
+    PetsComponent,
+    NewPetComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
